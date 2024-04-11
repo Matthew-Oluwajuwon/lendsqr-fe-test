5e5497eed8ae90bebf8969ed8d3dd7c89ec87153
Matarazy <macbook@Matarazy.local>
2024-04-11 16:54:36 +0100

    memoization of pages and components for optimization

--- src/common/layouts/Auth/AuthLayout.tsx
+++ src/common/layouts/Auth/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import logo from "../../../assets/images/logo.svg";
-import favicon from "../../../../favicon.png"
+import favicon from "../../../../favicon.png";
 import authImage from "../../../assets/images/login-img.svg";
 import "./AuthLayout.scss";
 import React from "react";
@@ -9,15 +9,23 @@ const Layout: React.FC = () => {
   return (
     <div className="auth-container">
       <section className="auth-container__left">
-        <img src={logo} alt="" />
-        <img src={authImage} alt="" className="auth-container__image" />
+        <img src={logo} alt="" className="" />
+        <img
+          src={authImage}
+          alt=""
+          className="auth-container__image loginImg"
+        />
       </section>
       <section className="auth-container__right">
-        <img src={favicon} alt="" className="auth-container__right__mobile-logo" />
+        <img
+          src={favicon}
+          alt=""
+          className="auth-container__right__mobile-logo"
+        />
         <Outlet />
       </section>
     </div>
   );
 };
 
-export const AuthLayout = React.memo(Layout)
+export const AuthLayout = React.memo(Layout);
